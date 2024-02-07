45f6a594a294a6d2a0861baa1d5316333cce5bb6
Marina Safina <safina531@gmail.com>
2024-02-06 18:54:22 -0600

    add load_json_file

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+from config import *
+import json
+from datetime import datetime
+
+
+def load_json_file():
+    with open(OPERATIONS, encoding="utf-8") as file:
+        json_user_operations = json.load(file)
+    return json_user_operations
+
